bb406d62e4197d9f2d6e723ef2c08613af4065b5
Harsh Parsaniya <154883212+HarshParsaniya@users.noreply.github.com>
2026-01-29 12:35:09 +0530

    Enhance App component with editingTodo state

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,14 +1,19 @@
-import "./App.css";
-import AddTodo from "./components/AddTodo";
-import Todos from "./components/Todos";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
 function App() {
+  const [editingTodo, setEditingTodo] = useState({id: null, text: ''})
   return (
     <>
       <h1>Learn about redux toolkit</h1>
-      <AddTodo />
-      <Todos />
+      <AddTodo editingTodo={editingTodo} setEditingTodo={setEditingTodo}/>
+      <Todos setEditingTodo={setEditingTodo}/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
