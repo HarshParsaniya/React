d87af75660223dee7d2b61248fb13d391804f255
Harsh Parsaniya <154883212+HarshParsaniya@users.noreply.github.com>
2026-01-29 12:35:45 +0530

    Refactor AddTodo component for editing functionality

--- reduxToolkitTodo/src/components/AddTodo.jsx
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,32 +1,29 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { addTodo, updateTodo } from "../features/todo/todoSlice";
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { addTodo, updateTodo } from '../features/todo/todoSlice'
 
-function AddTodo() {
-  const todos = useSelector(state => state.todos);
-  const [input, setInput] = useState("");
+function AddTodo({ editingTodo, setEditingTodo }) {
 
-  const dispatch = useDispatch();
+  const [input, setInput] = useState('')
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (editingTodo.id !== null) setInput(editingTodo.text);
+    else setInput('');
+  }, [editingTodo]);
 
   const addTodoHandler = (e) => {
-    e.preventDefault();
-    
-    if (!todos.completed) {
-      dispatch(addTodo(input));
+    e.preventDefault()
+
+    if (editingTodo.id !== null) {
+      dispatch(updateTodo({ id: editingTodo.id, text: input }));
+      setEditingTodo({ id: null, text: '' });
     } else {
-      dispatch(updateTodo(input));
+      dispatch(addTodo(input))
     }
 
-    setInput("");
-  };
-
-  const textFieldDecision = () => {
-    if (isTodoEditable) {
-      return input;
-    } else {
-      return todos.text;
-    }
-  };
+    setInput('')
+  }
 
   return (
     <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
@@ -34,71 +31,17 @@ function AddTodo() {
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         placeholder="Enter a Todo..."
-        // value={() => (todos.completed ? todos.text : input)}
         value={input}
         onChange={(e) => setInput(e.target.value)}
-        // readOnly={!isTodoEditable}
       />
-
       <button
         type="submit"
         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
       >
-        {todos.completed ? "Edit Todo" : "Add Todo"}
+        {editingTodo.id !== null ? 'Update Todo' : 'Add Todo'}
       </button>
     </form>
-  );
+  )
 }
 
-export default AddTodo;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, {useState} from 'react'
-// import {useDispatch} from 'react-redux'
-// import {addTodo} from '../features/todo/todoSlice'
-
-// function AddTodo() {
-
-//     const [input, setInput] = useState('')
-//     const dispatch = useDispatch()
-
-//     const addTodoHandler = (e) => {
-//         e.preventDefault()
-//         dispatch(addTodo(input))
-//         setInput('')
-//     }
-
-//   return (
-//     <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
-//       <input
-//         type="text"
-//         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-//         placeholder="Enter a Todo..."
-//         value={input}
-//         onChange={(e) => setInput(e.target.value)}
-//       />
-//       <button
-//         type="submit"
-//         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-//       >
-//         Add Todo
-//       </button>
-//     </form>
-//   )
-// }
-
-// export default AddTodo
+export default AddTodo
